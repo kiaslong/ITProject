6731988b477c466a6558199f199d0fe3b2b0485e
kiraslong <kiraslong@gmail.com>
2024-06-03 19:57:22 +0700

    feature: add header to home screen

--- lkrentapp/homeStackScreen/HomeScreen.js
+++ lkrentapp/homeStackScreen/HomeScreen.js
@@ -1,24 +1,82 @@
+import React , {useState} from "react";
+import { ScrollView, View, Image, StyleSheet, Text, Pressable, Alert } from "react-native";
+import { Ionicons } from '@expo/vector-icons';
 
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+
+function HeartIcon() {
+  const [isHeartPressed, setIsHeartPressed] = useState(false);
+
+  return (
+    <Pressable
+      onPress={() => setIsHeartPressed(!isHeartPressed)}
+    >
+      <Ionicons
+        name={isHeartPressed ? "heart" : "heart-outline"}
+        size={24}
+        color={isHeartPressed ? "red" : "black"}
+        style={styles.icon}
+      />
+    </Pressable>
+  );
+}
+
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Welcome to Lkrent App</Text>
-    </View>
+    <ScrollView
+      style={styles.container}
+      contentContainerStyle={styles.contentScrollView}
+    >
+      <View style={styles.headerHome}>
+        <Image
+          source={require("../assets/lkrentlogo.png")}
+          style={styles.headerImage}
+        />
+        <Text style={styles.headerText}>
+          Welcome,{"\n"}Phan Phi Long
+        </Text>
+        <View style={styles.iconContainer}>
+         <HeartIcon/>
+          <Pressable onPress={() => Alert.alert('Gift icon pressed')}>
+            <Ionicons name="gift-outline" size={24} color="black" style={styles.icon} />
+          </Pressable>
+        </View>
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
-  text: {
+  contentScrollView: {
+    flex: 1,
+  },
+  headerHome: {
+    paddingTop: 50,
+    paddingLeft: 20,
+    paddingRight: 20,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between"
+  },
+  headerText: {
     fontSize: 18,
-    color: '#000',
+    fontWeight: "heavy",
+    paddingLeft: 10,
+    flex: 1,
+  },
+  headerImage: {
+    height: 60,
+    width: 60,
+    borderRadius: 30,
+  },
+  iconContainer: {
+    flexDirection: "row",
+  },
+  icon: {
+    marginLeft: 18,
   },
 });
