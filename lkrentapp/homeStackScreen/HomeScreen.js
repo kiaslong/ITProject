e2d1eeebe186c76bc85dc143dcebfbe6a895c8d6
kiraslong <kiraslong@gmail.com>
2024-06-06 21:50:50 +0700

    feat: add promotion card and dot for index in homescreen

--- lkrentapp/homeStackScreen/HomeScreen.js
+++ lkrentapp/homeStackScreen/HomeScreen.js
@@ -1,17 +1,24 @@
-import React , {useState} from "react";
-import { ScrollView, View, Image, StyleSheet, Text, Pressable, Alert } from "react-native";
-import { Ionicons } from '@expo/vector-icons';
-import SearchBar from "../components/SearchBar"
+import React, { useState, useRef, useEffect } from "react";
+import {
+  ScrollView,
+  View,
+  Image,
+  StyleSheet,
+  Text,
+  Pressable,
+  Alert,
+  FlatList,
+  Dimensions,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import SearchBar from "../components/SearchBar";
+import PromotionCard from "../components/PromotionCard";
 
 function HeartIcon() {
- 
-
   return (
-    <Pressable
-    onPress={() => Alert.alert('Heart icon pressed')}
-    >
+    <Pressable onPress={() => Alert.alert("Heart icon pressed")}>
       <Ionicons
-        name= {"heart-outline"}
+        name={"heart-outline"}
         size={24}
         color={"black"}
         style={styles.icon}
@@ -20,29 +27,122 @@ function HeartIcon() {
   );
 }
 
+function FlatListForPromotion({ setCurrentIndex }) {
+  const onScroll = useRef((event) => {
+    const index = Math.round(
+      event.nativeEvent.contentOffset.x / (cardWidth + cardSpacing)
+    );
+    setCurrentIndex(index);
+  }).current;
+
+  return (
+    <View style={styles.flatListContainer}>
+      <FlatList
+        data={promotions}
+        renderItem={({ item }) => (
+          <View style={[styles.promotionCardWrapper, { width: cardWidth }]}>
+            <PromotionCard
+              imageUrl={item.imageUrl}
+              promotionText={item.promotionText}
+              discountText={item.discountText}
+            />
+          </View>
+        )}
+        keyExtractor={(item) => item.id}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+        snapToInterval={cardWidth + cardSpacing}
+        decelerationRate="fast"
+        contentContainerStyle={styles.promotionList}
+        snapToAlignment="start"
+        overScrollMode="never"
+        scrollToOverflowEnabled={false}
+        onScroll={onScroll}
+        scrollEventThrottle={16}
+        contentOffset={{ x: -cardSpacing, y: 0 }}
+        contentInset={{
+          up: 0,
+          down: 0,
+          left: 0,
+          right: width - cardWidth - cardSpacing * 2,
+        }}
+        ItemSeparatorComponent={() => <View style={{ width: cardSpacing }} />}
+      />
+    </View>
+  );
+}
+
+function DotIndex ({currentIndex}) {
+  return (
+    <View style={styles.dotsContainer}>
+    {promotions.map((_, index) => (
+      <View
+        key={index}
+        style={[
+          styles.dot,
+          { backgroundColor: index === currentIndex ? 'black' : 'grey' },
+        ]}
+      />
+    ))}
+  </View>
+  );
+}
+
+const promotions = [
+  {
+    id: "1",
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7Ow-jdSFfiCijZRPsQz6GQcoF61ahECtZMA&s",
+    promotionText: "Khuyến mãi 30% phí cho thuê xe Mercedes",
+    discountText: "30%",
+  },
+  {
+    id: "2",
+    imageUrl:
+      "https://cdni.autocarindia.com/utils/imageresizer.ashx?n=https://cms.haymarketindia.net/model/uploads/modelimages/BMW-2-Series-Gran-Coupe-271220221147.jpg&w=872&h=578&q=75&c=1",
+    promotionText: "Khuyến mãi 20% phí cho thuê xe BMW",
+    discountText: "20%",
+  },
+  {
+    id: "3",
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmIwj74aj-4g71TjdSxaNpLMTTO9CpBiUm5A&s",
+    promotionText: "Khuyến mãi thêm 10% cho người mới thuê xe lần đầu",
+    discountText: "10%",
+  },
+];
+
+const { width } = Dimensions.get("window");
+const cardWidth = width * 1.1;
+const cardSpacing = 8;
 
 export default function HomeScreen() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
   return (
-    <ScrollView
-      style={styles.container}
-      contentContainerStyle={styles.contentScrollView}
-    >
+    <ScrollView style={styles.container}>
       <View style={styles.headerHome}>
         <Image
           source={require("../assets/lkrentlogo.png")}
           style={styles.headerImage}
         />
-        <Text style={styles.headerText}>
-          Welcome,{"\n"}Phan Phi Long
-        </Text>
+        <Text style={styles.headerText}>Welcome,{"\n"}Phan Phi Long</Text>
         <View style={styles.iconContainer}>
-         <HeartIcon/>
-          <Pressable onPress={() => Alert.alert('Gift icon pressed')}>
-            <Ionicons name="gift-outline" size={24} color="black" style={styles.icon} />
+          <HeartIcon />
+          <Pressable onPress={() => Alert.alert("Gift icon pressed")}>
+            <Ionicons
+              name="gift-outline"
+              size={24}
+              color="black"
+              style={styles.icon}
+            />
           </Pressable>
         </View>
       </View>
       <SearchBar />
+      <Text style={styles.promotionText}>Chương trình khuyến mãi</Text>
+      <FlatListForPromotion setCurrentIndex={setCurrentIndex} />
+      <DotIndex currentIndex={currentIndex} />
     </ScrollView>
   );
 }
@@ -52,20 +152,17 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
   },
-  contentScrollView: {
-    flex: 1,
-  },
   headerHome: {
     paddingTop: 80,
     paddingLeft: 20,
     paddingRight: 20,
     flexDirection: "row",
     alignItems: "center",
-    justifyContent: "space-between"
+    justifyContent: "space-between",
   },
   headerText: {
     fontSize: 18,
-    fontWeight: "heavy",
+    fontWeight: "bold",
     paddingLeft: 10,
     flex: 1,
   },
@@ -79,5 +176,33 @@ const styles = StyleSheet.create({
   },
   icon: {
     marginLeft: 18,
+  },
+  promotionText: {
+    marginLeft: 25,
+    fontSize: 20,
+    fontWeight: "bold",
+    padding: 2,
+  },
+  flatListContainer: {
+    height: 185, 
+  },
+  promotionList: {
+    paddingHorizontal: cardSpacing,
+  },
+  promotionCardWrapper: {
+    borderRadius: 10,
+    overflow: "hidden",
+    height: 205, 
+  },
+  dotsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  dot: {
+    width: 8,
+    height: 8,
+    borderRadius: 4,
+    marginHorizontal: 4,
   },
 });
--- lkrentapp/notiStackScreen/NotificationScreen.js
+++ lkrentapp/notiStackScreen/NotificationScreen.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet, Dimensions,Alert, ScrollView,FlatList } from "react-native";
-
+import { View, Text, StyleSheet, Dimensions, Alert, FlatList } from "react-native";
 import NotificationBox from "../components/NotificationBox";
 
 const sampleData = [
@@ -18,42 +17,35 @@ const sampleData = [
     message: "Bạn đã thanh toán thành công cho đơn hàng thuê xe",
     time: "1 giờ trước",
   },
-
 ];
 
 export default function NotiScreen() {
-
   const handlePress = (title) => {
     Alert.alert('Notification Pressed', `You pressed the notification: ${title}`);
   };
 
   const renderItem = ({ item }) => (
-      <NotificationBox
-        icon={item.icon}
-        title={item.title}
-        message={item.message}
-        time={item.time}
-        onPress={() => handlePress(item.title)}
-      />
+    <NotificationBox
+      icon={item.icon}
+      title={item.title}
+      message={item.message}
+      time={item.time}
+      onPress={() => handlePress(item.title)}
+    />
   );
 
-
-
   return (
-    <ScrollView>
-    <View style={styles.container}>
-      <View style={styles.titleContainer}>
-        <Text style={styles.title}>Thông báo</Text>
-      </View>
-      <View style={styles.notiContainer}>
-      <FlatList
-        data={sampleData}
-        renderItem={renderItem}
-        keyExtractor={item => item.id}
-      />
-      </View>
-    </View>
-    </ScrollView>
+    <FlatList
+      data={sampleData}
+      renderItem={renderItem}
+      keyExtractor={item => item.id}
+      ListHeaderComponent={() => (
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>Thông báo</Text>
+        </View>
+      )}
+      contentContainerStyle={styles.container}
+    />
   );
 }
 
@@ -61,14 +53,13 @@ const deviceHeight = Dimensions.get("window").height;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: "center",
     paddingTop: deviceHeight < 1000 ? 80 : 100,
     backgroundColor: "#fff",
+    flexGrow: 1, 
+    marginHorizontal:10,
   },
   titleContainer: {
     width: "100%",
-    position: "relative",
     alignItems: "center",
     paddingVertical: deviceHeight < 1000 ? 10 : 20,
   },
@@ -79,10 +70,10 @@ const styles = StyleSheet.create({
     color: "#03a9f4",
   },
   notiContainer: {
-    flex:1,
-    width:"100%",
-    flexDirection:"column",
-    padding:15,
+    flex: 1,
+    width: "100%",
+    flexDirection: "column",
+    padding: 15,
     backgroundColor: '#fff',
   },
 });
